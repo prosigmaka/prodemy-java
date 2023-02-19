ca6291c72da7077eb1035895fe8868c115a9d3e6
prodemy <prodemy@192.168.56.1>
2023-02-19 22:53:00 +0700

    Add simple-datatype-project

---
+++ simple-datatype-project/src/com/prodemy/TipeDataSederhana.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package com.prodemy;
+
+/**
+ * @author prodemy
+ *
+ */
+public class TipeDataSederhana {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// int, long, double, float, byte, short
+		
+		int x = 10;
+		long l = 10L;
+		double d = 10.5;
+		
+		System.out.println("Nilai x:"+(x+10));
+		System.out.println("Nilai l:"+l);
+		System.out.println("Nilai d:"+d);
+		
+		String s = "Hello";
+		System.out.println("Isi s:"+s);
+		
+		s = s + " prodemy";
+		System.out.println("Isi s:"+s);
+	}
+
+}
